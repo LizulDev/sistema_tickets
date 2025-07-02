8d63ab84587b401a8fde32c86e48aedf03ea89f8
Luiz Felipe Kauê da Silva <130705905+LizulDev@users.noreply.github.com>
2025-07-02 20:04:14 -0300

    Update conexaobd.php

--- php/conexaobd.php
+++ php/conexaobd.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db = 'atividade_final_venda_tickets';
-$user = 'root';
-$pass = 'root';
+$user = '----';
+$pass = '----';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
